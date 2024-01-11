37759a80a7c9291917fd899f83c6cf72bfbb9b95
FerhatTasci <ferhat95t@gmail.com>
2024-01-11 23:11:20 +0100

    ajout ferhat

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,14 @@
 // src/Controller/HomeController.php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Products;
+use App\Entity\Customers;
+use App\Repository\ProductsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\ProductsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -18,5 +22,19 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'products' => $products,
         ]);
+    }
+
+    #[Route('/save-product/{productId}', name: 'save_product_to_customer', methods: ['POST'])]
+    public function saveProductToCustomer(Request $request, EntityManagerInterface $entityManager, Products $product): Response
+    {
+        $user = $this->getUser(); // Make sure you have a method to get the currently logged-in user
+        if (!$user) {
+            // Redirect to the login page if the user is not logged in
+            return $this->redirectToRoute('app_login');
+        }
+
+
+        // Redirigez vers la page précédente ou vers la page 'home'
+        return $this->redirectToRoute('app_home');
     }
 }
